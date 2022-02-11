da3758606f01996e5aa1924b4233529c1dacc518
LyubenMurdzhev <36411453+LyubenMurdzhev@users.noreply.github.com>
2022-02-11 12:40:57 +0200

    Add files via upload

--- myJS.js
+++ myJS.js
@@ -3,7 +3,7 @@ const
     onlyLowerCase = "abcdefghijklmnopqrstuvwxyz",
     onlyUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     onlyNumbers = "0123456789",
-    onlySpecialCharacters = "~`! @#$%^&*()_-+={[}]|\:;\"'<,>.?/",
+    onlySpecialCharacters = "~`!@#$%^&*()_-+={[}]|\:;\"'<,>.?/",
 
     // Checkboxes
     upperCaseCheckbox = document.getElementById("upperCaseCheckbox"),
@@ -15,9 +15,8 @@ const
     // Elements of the page
     generateBtn = document.getElementById("btn-generate"),
     slider = document.getElementById("length"),
-    copyBtn = document.getElementById("copyBtn");
-
-
+    copyBtn = document.getElementById("copyBtn"),
+    checkboxes = document.querySelectorAll('.checkboxes');
 
 // Generate password function
 const generatePassword = (e) => {
@@ -25,29 +24,67 @@ const generatePassword = (e) => {
 
     var numberOfCharacters = document.getElementById("numberOfCharacters").value, // Password length 
         generatedPassword = "", // Empty string for the generated password
-        arrayOfCharacters = ""; // Array of characters to use
+        arrayOfCharacters = "",// Array of characters to use
+        securityIndex = 0, // Security index
+        securityIndicator = document.getElementById("securityIndicator"), //Security indicator field
+        dummy=0; 
+     
 
-    //Adding arrays depending on checked heckboxes
-    if (upperCaseCheckbox.checked == true) {
-        arrayOfCharacters += onlyUpperCase;
-    } if (lowerCaseCheckbox.checked == true) {
-        arrayOfCharacters += onlyLowerCase;
-    } if (numbersCheckbox.checked == true) {
-        arrayOfCharacters += onlyNumbers;
-    } if (specialCharactersCheckbox.checked == true) {
-        arrayOfCharacters += onlySpecialCharacters;
+    //Adding arrays depending on checked heckboxes and assigning value to securityIndex
+    upperCaseCheckbox.checked == true ?
+    (arrayOfCharacters += onlyUpperCase) & (securityIndex++)
+        : false;
+    
+    lowerCaseCheckbox.checked == true ?
+        (arrayOfCharacters += onlyLowerCase) & (securityIndex++) 
+        : false;
+
+   numbersCheckbox.checked == true ?
+        (arrayOfCharacters += onlyNumbers) & (securityIndex++)
+        : false;
+
+    specialCharactersCheckbox.checked == true ?
+        (arrayOfCharacters += onlySpecialCharacters) & (securityIndex++)
+        : false;
+  
+
+    // Adjust securityIndex depending on password  length
+    if (numberOfCharacters <= 8) { // + 1 Security Index if password is more then 24 characters
+        securityIndex = (securityIndex -2);
+    } else if (numberOfCharacters <= 12) {
+        securityIndex = (securityIndex -1);
+    } else if (numberOfCharacters <= 24) {
+        securityIndex++;
     }
 
-    // Generating the password
-    for (var i = 0; i < numberOfCharacters; i++) {
-        var randomNumber = Math.floor(Math.random() * arrayOfCharacters.length);
-        generatedPassword += arrayOfCharacters.substring(randomNumber, randomNumber + 1);
+
+    // Function to check password security
+    if (securityIndex >= 4) {
+        securityIndicator.innerHTML = "Password is strong";
+    } else if (securityIndex == 3) {
+        securityIndicator.innerHTML = "Password is medium";
+    } else {
+        securityIndicator.innerHTML = "Password is weak";
     }
 
-    let passwordField = document.getElementById("passwordField").value = generatedPassword; // Input to the new password
+    console.log(securityIndex);
+
+
+
+        // Generating the password
+        for (let i = 0; i < numberOfCharacters; i++) {
+            let randomNumber = Math.floor(Math.random() * arrayOfCharacters.length);
+            generatedPassword += arrayOfCharacters.substring(randomNumber, randomNumber + 1);
+        }
+    
+        let passwordField = document.getElementById("passwordField").value = generatedPassword; // Input to the new password
 }
-generateBtn.addEventListener('click', generatePassword);
-slider.addEventListener('change', generatePassword); // slider also generates password
+generateBtn.addEventListener('click', generatePassword); // Generate button triggers Password Generation
+slider.addEventListener('change', generatePassword); // slider triggers Password Generation
+
+document.querySelectorAll('.checkboxes').forEach(item => { // Checkbox change triggers Password Generation
+    item.addEventListener('change', generatePassword);
+  }) 
 
 
 // Copy to clipboard function
@@ -55,6 +92,7 @@ copyPassword = (e) => {
     e.preventDefault;
     let passwordField = document.getElementById("passwordField").value;
 
+    if (passwordField !== "") { // If password field is not empty
     navigator.clipboard.writeText(passwordField).then(
         function () {
             /* clipboard successfully set */
@@ -64,6 +102,10 @@ copyPassword = (e) => {
             /* clipboard write failed */
             window.alert('Opps! Your browser does not support the Clipboard API')
         }
-    )
+    ) 
+    } else { // If password field is empty
+        window.alert('Please generate a password before you copy!')
+    }
 }
-copyBtn.addEventListener("click", copyPassword);
+copyBtn.addEventListener("click", copyPassword); // Adding event listener to Copy button
+
