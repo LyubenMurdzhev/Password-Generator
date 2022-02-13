26633ff73590868ce686523ec3c83ee58c694009
LyubenMurdzhev <36411453+LyubenMurdzhev@users.noreply.github.com>
2022-02-13 20:56:26 +0200

    Add files via upload

--- myJS.js
+++ myJS.js
@@ -10,13 +10,15 @@ const
     lowerCaseCheckbox = document.getElementById("lowerCaseCheckbox"),
     numbersCheckbox = document.getElementById("numberCheckbox"),
     specialCharactersCheckbox = document.getElementById("specialCharactersCheckbox"),
+    repeatingCharactersCheckbox = document.getElementById("repeatingCharactersCheckbox"),
 
 
-    // Elements of the page
+    // Elements of the page  (const)
     generateBtn = document.getElementById("btn-generate"),
     slider = document.getElementById("length"),
     copyBtn = document.getElementById("copyBtn"),
     checkboxes = document.querySelectorAll('.checkboxes');
+
 
 // Generate password function
 const generatePassword = (e) => {
@@ -24,60 +26,84 @@ const generatePassword = (e) => {
 
     var numberOfCharacters = document.getElementById("numberOfCharacters").value, // Password length 
         generatedPassword = "", // Empty string for the generated password
-        arrayOfCharacters = "",// Array of characters to use
+        stringOfCharacters = "",// Array of characters to use
         securityIndex = 0, // Security index
-        securityIndicator = document.getElementById("securityIndicator"), //Security indicator field
-        dummy=0; 
-     
+        securityIndicator = document.getElementById("securityIndicator"); //Security indicator field
+       
 
-    //Adding arrays depending on checked heckboxes and assigning value to securityIndex
+    //Adding arrays depending on checked checkboxes and assigning value to securityIndex
     upperCaseCheckbox.checked == true ?
-    (arrayOfCharacters += onlyUpperCase) & (securityIndex++)
+    (stringOfCharacters += onlyUpperCase) & (securityIndex++)
         : false;
     
     lowerCaseCheckbox.checked == true ?
-        (arrayOfCharacters += onlyLowerCase) & (securityIndex++) 
+        (stringOfCharacters += onlyLowerCase) & (securityIndex++) 
         : false;
 
    numbersCheckbox.checked == true ?
-        (arrayOfCharacters += onlyNumbers) & (securityIndex++)
+        (stringOfCharacters += onlyNumbers) & (securityIndex++)
         : false;
 
     specialCharactersCheckbox.checked == true ?
-        (arrayOfCharacters += onlySpecialCharacters) & (securityIndex++)
+        (stringOfCharacters += onlySpecialCharacters) & (securityIndex++)
         : false;
-  
+    
+    repeatingCharactersCheckbox.checked == true ?
+        (securityIndex--)
+        :false;
 
-    // Adjust securityIndex depending on password  length
-    if (numberOfCharacters <= 8) { // + 1 Security Index if password is more then 24 characters
+    // Adjust securityIndex depending on password length
+    if (numberOfCharacters <= 8) { 
         securityIndex = (securityIndex -2);
     } else if (numberOfCharacters <= 12) {
         securityIndex = (securityIndex -1);
-    } else if (numberOfCharacters <= 24) {
-        securityIndex++;
+    } else if (numberOfCharacters >= 13) {
+        securityIndex = securityIndex + 2;
     }
 
-
-    // Function to check password security
-    if (securityIndex >= 4) {
-        securityIndicator.innerHTML = "Password is strong";
-    } else if (securityIndex == 3) {
-        securityIndicator.innerHTML = "Password is medium";
+    // Function to check password security and add the respective class
+    if (securityIndex >= 5) {
+        securityIndicator.innerHTML = "strong";
+        securityIndicator.classList.add("strong");
+        securityIndicator.classList.remove("medium","weak");
+    } else if (securityIndex >= 3) {
+        securityIndicator.innerHTML = "medium";
+        securityIndicator.classList.add("medium");
+        securityIndicator.classList.remove("strong","weak");
     } else {
-        securityIndicator.innerHTML = "Password is weak";
+        securityIndicator.innerHTML = "weak";
+        securityIndicator.classList.add("weak");
+        securityIndicator.classList.remove("medium","strong");
     }
 
     console.log(securityIndex);
 
-
-
-        // Generating the password
-        for (let i = 0; i < numberOfCharacters; i++) {
-            let randomNumber = Math.floor(Math.random() * arrayOfCharacters.length);
-            generatedPassword += arrayOfCharacters.substring(randomNumber, randomNumber + 1);
-        }
-    
-        let passwordField = document.getElementById("passwordField").value = generatedPassword; // Input to the new password
+        // Generating the password without Permutation
+    generator = () => {
+            if (repeatingCharactersCheckbox.checked == true) { // checkbox 'Exclude repeating characters' is checked TRUE
+                var arrayOfGeneratedCharacters =[]; // Array to push generated characters
+           
+                do {
+                    var randomNumber = Math.floor(Math.random() * stringOfCharacters.length);
+                    // generatedPassword += stringOfCharacters.substring(randomNumber, randomNumber + 1);
+                    var generatedCharacter = stringOfCharacters.substring(randomNumber, randomNumber + 1);
+                    if (!arrayOfGeneratedCharacters.includes(generatedCharacter)){ // Check if the generated character is already in the Array
+                        arrayOfGeneratedCharacters.push(generatedCharacter);
+                    }
+                } while (arrayOfGeneratedCharacters.length < numberOfCharacters); // End conditional check
+                let generatedPassword = arrayOfGeneratedCharacters.join("");
+                let passwordField = document.getElementById("passwordField").value = generatedPassword; // Displays in password field
+            
+            } 
+            else { // checkbox 'Exclude repeating characters' is checked FALSE
+                for (let i = 0; i < numberOfCharacters; i++) {
+                    let randomNumber = Math.floor(Math.random() * stringOfCharacters.length);
+                    generatedPassword += stringOfCharacters.substring(randomNumber, randomNumber + 1); 
+                    let passwordField = document.getElementById("passwordField").value = generatedPassword; // Displays in password field
+                }  
+            }
+        } 
+    generator();
 }
 generateBtn.addEventListener('click', generatePassword); // Generate button triggers Password Generation
 slider.addEventListener('change', generatePassword); // slider triggers Password Generation
@@ -108,4 +134,8 @@ copyPassword = (e) => {
     }
 }
 copyBtn.addEventListener("click", copyPassword); // Adding event listener to Copy button
+
+
+
+
 
